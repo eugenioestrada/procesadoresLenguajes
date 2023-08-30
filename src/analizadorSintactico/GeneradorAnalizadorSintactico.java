3bb23b1100350e0840c8f0cb58ff36169a41b043
Eugenio Estrada <eugenio.estrada@gmail.com>
2023-08-30 19:40:09 +0200

    Primera versión analizador sintáctico

--- src/analizadorSintactico/GeneradorAnalizadorSintactico.java
+++ src/analizadorSintactico/GeneradorAnalizadorSintactico.java
@@ -1,5 +1,8 @@
 package analizadorSintactico;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class GeneradorAnalizadorSintactico {
     public static void main(String[] args) {
         String ficheroEspecificacion = System.getProperty("user.dir") + "\\src\\analizadorSintactico\\pocket.cup";
--- src/analizadorSintactico/PruebaAnalizadorSintactico.java
+++ src/analizadorSintactico/PruebaAnalizadorSintactico.java
@@ -1,19 +1,23 @@
 package analizadorSintactico;
 
+import java_cup.runtime.Scanner;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class PruebaAnalizadorSintactico {
     public static void main(String[] args) {
         String ficheroEntrada = "prueba1.pck";
-        String directorio = System.getProperty("user.dir") + "\\src\\examples\\";
+        String directorio = System.getProperty("user.dir") + "\\examples\\";
 
         String resultado = "";
 
         try {
             BufferedReader lector = new BufferedReader(new FileReader(directorio + ficheroEntrada));
 
-            //Scanner myScanner = new Yylex(lector);
+            Scanner myScanner = new Yylex(lector);
+            parser myParser = new parser(myScanner);
+            myParser.parse();
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
